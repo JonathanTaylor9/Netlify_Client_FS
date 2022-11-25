83083e8d66d6242ad3274f23cfca2c997a2a520b
JonathanTaylor9 <65346162+JonathanTaylor9@users.noreply.github.com>
2022-11-25 09:49:20 +0000

    Putting app.js back to heroku

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   const [listOfPosts, setListOfPosts] = useState([]);
 
   useEffect(() => {
-    axios.get("https://adcadb1.azurewebsites.net/posts").then((response) => {
-// Below config is for Heroku
-//    axios.get("https://full-stack-api-jt.herokuapp.com/posts").then((response) => {
+    axios.get("https://full-stack-api-jt.herokuapp.com/posts").then((response) => {
 // This line was just used for testing      console.log(response.data);
       setListOfPosts(response.data);
     });
@@ -29,5 +27,3 @@ function App() {
 }
 
 export default App;
-
-//nonesense to trigger git
